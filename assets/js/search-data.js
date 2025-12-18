430c684dabbf3edf75da5cf7a4580ed3aabe4176
haozhetan <140185638+haozhetan@users.noreply.github.com>
2025-12-18 14:05:02 +0000

    Deploying to gh-pages from @ haozhetan/haozhetan.github.io@11e60fa6f386147a1103acbc299ffc185466f970 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -73,13 +73,6 @@ ninja.data = [{
           window.open("https://github.com/haozhetan", "_blank");
         },
       },{
-        id: 'social-linkedin',
-        title: 'LinkedIn',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.linkedin.com/in/haozhe-tan-162424261", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
